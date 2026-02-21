43eb4e7f41c6c2adf97b503409b60eb579b659d7
rgardler-msft <ross.gardler@microsoft.com>
2026-02-21 01:04:19 -0800

    TF-0MLW392IX1IOR7R0: Exclude .worklog/ from Vitest file discovery

--- vitest.e2e.config.ts
+++ vitest.e2e.config.ts
@@ -11,5 +11,10 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["**/*.e2e.test.*"],
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/.worklog/**",
+    ],
   },
 });
